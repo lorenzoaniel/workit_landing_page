614790f462756ce22e86f2a02f25244520d4f4ce
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-18 23:06:17 -0600

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import postcssImport from "postcss-import";
 import tailwindcss from "tailwindcss";
+import { resolve } from "path";
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(readFileSync(resolve(__dirname, "package.json"), "utf-8"));
+const homepage = pkg.homepage;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: process.env.NODE_ENV === "production" ? homepage : "/",
 	plugins: [react(), postcssImport(), tailwindcss("./tailwind.config.js")],
 });
